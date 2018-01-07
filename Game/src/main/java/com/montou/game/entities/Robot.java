05b412b95aa6ea8ec19eaf37e40d2e800056900e
HugoF <hugo.fourmy@gmail.com>
2018-01-07 17:05:26 +0100

    Modif Robot.java + TU

--- Game/src/main/java/com/montou/game/entities/Robot.java
+++ Game/src/main/java/com/montou/game/entities/Robot.java
@@ -3,87 +3,104 @@ package com.montou.game.entities;
 import java.io.Serializable;
 
 public class Robot implements Serializable {
-	
+
 	private String name;
-	
+
 	private int currentLine;
 	private int currentColumn;
-	
+
+	private int maxLifePoint;
 	private int lifePoints;
+	private int maxEnergyPoint;
 	private int energyPoints;
 
 	public Robot(String name, int startLine, int startColumn) {
 		this.name = name;
 		this.currentLine = startLine;
 		this.currentColumn = startColumn;
-		
+
+		this.maxLifePoint = 100;
 		this.lifePoints = 100;
+		this.maxEnergyPoint = 100;
 		this.energyPoints = 100;
 	}
-	
+
 	public String getName() {
 		return this.name;
 	}
-	
+
 	public int getCurrentLine() {
 		return this.currentLine;
 	}
-	
+
 	public void setCurrentLine(int line) {
 		this.currentLine = line;
 	}
-	
+
 	public int getCurrentColumn() {
 		return this.currentColumn;
 	}
-	
+
 	public void setCurrentColumn(int column) {
 		this.currentColumn = column;
 	}
-	
+
 	public int getLifePoints() {
 		return this.lifePoints;
 	}
-	
+
 	public void substractLifePoints(int value) {
-		this.lifePoints -= value;
+		if (value > 0)
+			this.lifePoints -= value;
+		else
+			value = 0;
 		if (this.lifePoints < 0)
 			this.lifePoints = 0;
-		
-		System.out.println(String.format("Le joueur %s a perdu %s point(s) de vie (%s/100) !", this.name, value, this.lifePoints));
+		System.out.println(String.format("Le joueur %s a perdu %s point(s) de vie (%s/%s) !", this.name, value,
+				this.lifePoints, this.maxLifePoint));
 	}
-	
+
 	public int getEnergyPoints() {
 		return this.energyPoints;
 	}
-	
+
 	public void substractEnergyPoints(int value) {
-		this.energyPoints -= value;
+		if (value > 0)
+			this.energyPoints -= value;
+		else
+			value = 0;
 		if (this.energyPoints < 0)
 			this.energyPoints = 0;
-		
-		System.out.println(String.format("Le joueur %s a perdu %s point(s) d'énergie (%s/100) !", this.name, value, this.energyPoints));
+		System.out.println(String.format("Le joueur %s a perdu %s point(s) d'énergie (%s/%s) !", this.name, value,
+				this.energyPoints, this.maxEnergyPoint));
 	}
-	
+
 	public void regen(double ratePerTurn) {
-		int sum = (int)(2 * ratePerTurn);
-		
+		// int sum = (int) (2 * ratePerTurn);
+		int sum;
+
+		if (ratePerTurn > 0)
+			sum = (int) ratePerTurn;
+		else
+			sum = 0;
+
 		if (this.energyPoints < 100)
 			this.energyPoints += sum;
-		
+
 		if (this.energyPoints > 100)
 			this.energyPoints = 100;
-		
-		System.out.println(String.format("Le joueur %s a récupéré %s points d'énergie (%s/100) !", this.name, sum, this.energyPoints));
+
+		System.out.println(String.format("Le joueur %s a récupéré %s points d'énergie (%s/100) !", this.name, sum,
+				this.energyPoints));
 	}
-	
+
 	public boolean isActive() {
 		return this.lifePoints > 0;
 	}
-	
+
 	@Override
 	public String toString() {
-		return String.format("Name : %s, Line : %s, Column : %s, LifePoints : %s, EnergyPoints : %s, IsActive : %s", this.name,
-				this.currentLine, this.currentColumn, this.lifePoints, this.energyPoints, this.isActive());
+		return String.format("Name : %s, Line : %s, Column : %s, LifePoints : %s, EnergyPoints : %s, IsActive : %s",
+				this.name, this.currentLine, this.currentColumn, this.lifePoints, this.energyPoints, this.isActive());
 	}
 }
---
+++ Game/src/main/java/com/montou/game/test/TestRobot.java
@@ -0,0 +1,57 @@
+package com.montou.game.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.montou.game.entities.Robot;
+
+public class TestRobot {
+
+	Robot robot1;
+	Robot robot2;
+	Robot robot3;
+
+	@Before
+	public void setup() {
+		robot1 = new Robot("robot1", 1, 1);
+		robot2 = new Robot("robot2", 2, 2);
+		robot3 = new Robot("robot3", 3, 3);
+	}
+
+	@Test
+	public void substractLifePointsTest() {
+		// test avec valeur négative, positive, et si le robot passe en dessous de 0
+
+		robot1.substractLifePoints(-20);
+		robot2.substractLifePoints(20);
+		robot2.substractLifePoints(-10);
+		robot3.substractLifePoints(120);
+		assertArrayEquals(new int[] { robot1.getLifePoints(), robot2.getLifePoints(), robot3.getLifePoints() },
+				new int[] { 100, 80, 0 });
+	}
+
+	@Test
+	public void substractEnergyPointsTest() {
+		robot1.substractEnergyPoints(-20);
+		robot2.substractEnergyPoints(20);
+		robot2.substractEnergyPoints(-10);
+		robot3.substractEnergyPoints(120);
+		assertArrayEquals(new int[] { robot1.getEnergyPoints(), robot2.getEnergyPoints(), robot3.getEnergyPoints() },
+				new int[] { 100, 80, 0 });
+
+	}
+
+	@Test
+	public void regenTest() {
+		robot1.substractEnergyPoints(20);
+		robot1.regen(10);
+		robot2.substractEnergyPoints(20);
+		robot2.regen(-20);
+		robot3.regen(200);
+
+		assertArrayEquals(new int[] { robot1.getEnergyPoints(), robot2.getEnergyPoints(), robot3.getEnergyPoints() },
+				new int[] { 90, 80, 100 });
+	}
+}
